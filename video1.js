d45f76f7f3b6df68590c7603e2b1361b2eeb5dda
Gustavo Caetano <gustavo.caetano13@gmail.com>
2022-09-11 21:42:59 -0300

    Adiciona hofs-emojis e atualiza video1 com exemplo de forEach()

--- video1.js
+++ video1.js
@@ -17,8 +17,13 @@ console.log(exemple(5, 4));
 //========================================
 
 //ARROW FUNCTION
-const arrowFunction = (num1, num2) => {
-  return num1 + num2;
-};
+const arrowFunction = (num1, num2) => num1 + num2;
 
 console.log(arrowFunction(10, 8));
+
+//========================================
+
+//HOFS - HIGHER ORDER FUNCTIONS
+const array = ['GOKU', 'GOHAN', 'BULMA', 'MESTRE KAME'];
+
+array.forEach((personagem) => console.log(`DBZ - ${personagem}`));
